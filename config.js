4ad2f3adcceb999c302d3b10503290e16a720f5f
Stratto Plataform - <76053349+saiadasdividas@users.noreply.github.com>
2025-07-02 20:22:41 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js — Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgg5jnDEypyXsAxgqFHDZQzK3rpLY3nU8",
+  authDomain: "plataforma-de-vendas-a87c2.firebaseapp.com",
+  projectId: "plataforma-de-vendas-a87c2",
+  storageBucket: "plataforma-de-vendas-a87c2.appspot.com",
+  messagingSenderId: "177158177496",
+  appId: "1:177158177496:web:88d1c178147e7e91064dad"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
